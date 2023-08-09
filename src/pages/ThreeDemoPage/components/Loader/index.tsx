882d2a85ffb1e77bdbb4927485581fa0b39724ac
hejinlin <769316680@qq.com>
2023-08-09 17:53:21 +0800

    feat: loader

--- src/pages/ThreeDemoPage/components/Loader/index.tsx
+++ src/pages/ThreeDemoPage/components/Loader/index.tsx
@@ -1,12 +1,15 @@
 import { useProgress } from "@react-three/drei";
 import { Plane } from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
 export default function Loader() {
   const { progress } = useProgress();
+  const { scale } = useSpring({
+    scale: progress,
+  });
   return (
-    <mesh>
+    <animated.mesh scale-x={scale} scale-y={0.1}>
       <Plane args={[5, 0.1]} />
       <meshBasicMaterial color="blue" />
-      <mesh scale={[progress, 1, 1]} />
-    </mesh>
+    </animated.mesh>
   );
 }
